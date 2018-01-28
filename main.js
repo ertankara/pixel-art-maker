9524879d6a4e1d50766580192bd09509b1014b68
Ertan Kara <kara.ertan9@gmail.com>
2018-01-28 19:40:37 +0300

    Complete styles, ready to color or decolor cells

---
+++ main.js
@@ -0,0 +1,52 @@
+const $hiddenArea = document.getElementById('hidden-area');
+const $expand = document.getElementById('expand');
+const $table = document.getElementById('create-table');
+const $submit = document.getElementById('submit-button');
+const $width = document.getElementById('input-width');
+const $height = document.getElementById('input-height');
+const $displayWidth = document.getElementById('realtime-width');
+const $displayHeight = document.getElementById('realtime-height');
+
+
+// show real time change of the width value
+$width.addEventListener('change', e => {
+  console.log('trinket');
+  $displayWidth.innerHTML = e.target.value;
+});
+
+// show real time change of the height value
+$height.addEventListener('change', e => {
+  $displayHeight.innerHTML = e.target.value;
+});
+
+
+// when hovering on 'psst'
+$hiddenArea.addEventListener('mouseover', e => {
+  $hiddenArea.style.bottom = '0px';
+    $expand.style.opacity = '0';
+});
+
+// when hovering stops
+$hiddenArea.addEventListener('mouseleave', e => {
+  $hiddenArea.style.bottom = '155px';
+  setTimeout(() => {
+    $expand.style.opacity = '1';
+  }, 200);
+});
+
+// when 'create' button is clicked
+$submit.addEventListener('click', e => {
+  $table.style.visibility = 'visible';
+
+  /* this is the part that creates table */
+  let width = $width.value;
+  let height = $height.value;
+  let row, cell;
+  console.log(width, height);
+  for (let i = 0; i < height; i++) {
+    row = $table.insertRow(i);
+    for (let j = 0; j < width; j++) {
+      row.insertCell(j);
+    }
+  }
+});
